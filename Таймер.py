bb1fe23328a206aaf934d0e91d701692347961fd
Юсуф <guseinov709@mail.ru>
2025-08-15 01:45:58 +0300

    Add files via upload

---
+++ Таймер.py
@@ -0,0 +1,41 @@
+import time
+import help_meneger as s
+import sys
+
+def countdown(minutes):
+    seconds = minutes * 60
+    while seconds > 0:
+        mins, secs = divmod(seconds, 60)
+        print(f"\rОсталось: {mins:02d}:{secs:02d}", end="")
+        time.sleep(1)
+        seconds -= 1
+    print("\n⏰ Время вышло!")
+    try:
+        s.meneger.play_music(r"C:\Users\Yusuf\OneDrive\Документы\Scan\Пайтон\basic-alarm-ringtone.mp3")
+    except:
+        s.meneger.say('Время вышло!')
+    sys.exit(0)
+
+if __name__ == "__main__":
+    try:
+        minutes_input = input('Введите количество минут для таймера: ')
+        
+        # Извлекаем только цифры из ввода
+        digits = [d for d in minutes_input if d.isdigit()]
+        
+        if not digits:
+            print("Ошибка: введите хотя бы одну цифру")
+        else:
+            minutes_num = int(''.join(digits))
+            
+            if minutes_num <= 0:
+                print("Ошибка: число минут должно быть положительным")
+            else:
+                print(f"Таймер запущен на {minutes_num} минут(ы)")
+                countdown(minutes_num)
+                
+    except ValueError:
+        print("Ошибка: введите корректное число минут")
+    except KeyboardInterrupt:
+        print("\nТаймер отменён")
+        sys.exit(0)
